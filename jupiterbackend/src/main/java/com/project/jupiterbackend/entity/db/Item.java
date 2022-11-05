d7daed632adad93b0bd015935259745972eb715b
LuminescentOrange <qiyuanc3@illinois.edu>
2022-11-05 14:02:55 -0500

    Hibernate and Database Setup

--- jupiterbackend/src/main/java/com/project/jupiterbackend/entity/db/Item.java
+++ jupiterbackend/src/main/java/com/project/jupiterbackend/entity/db/Item.java
@@ -1,37 +1,58 @@
 package com.project.jupiterbackend.entity.db;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity //map class to table
+@Table(name = "items") // map database table name
 @JsonIgnoreProperties(ignoreUnknown = true) //可以ignore不认识的
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Item implements Serializable { //传给别人 打个包  寸硬盘 etc
+public class Item implements Serializable { //传给别人 打个包  存硬盘 etc
 
+    //https://www.baeldung.com/java-serial-version-uid
+    private static final long serialVersionUID = 1L;
+    //做了改变之后，hibernate识别version number有没有变，需不需要更新
+    @Id // primary key
     @JsonProperty("id")
-    private String id;
+    private String id; //没有@Column是因为名字和database里一样
 
     @JsonProperty("title")
     private String title;
-
     @JsonProperty("url")
     private String url;
-
+    @Column(name = "thumbnail_url")
     @JsonProperty("thumbnail_url")
     private String thumbnailUrl;
 
+    @Column(name = "broadcaster_name")
     @JsonProperty("broadcaster_name")
     @JsonAlias({ "user_name" })
     private String broadcasterName;
 
+
+    @Column(name = "game_id")
     @JsonProperty("game_id")
     private String gameId;
-
+    @Enumerated(value = EnumType.STRING)
     @JsonProperty("item_type")
     private ItemType type;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "itemSet") //user
+    private Set<User> users= new HashSet<>();
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
 
     public String getId() {
         return id;
